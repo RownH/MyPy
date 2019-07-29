b3769eac34d53e0118e8c537a32e6d3a6154f486
rownh <799698579@qq.com>
2019-07-29 21:07:44 +0800

    python求概率问题

---
+++ damo1.py
@@ -0,0 +1,63 @@
+from random import randint
+def Probability(n,m,k):
+    num={"red":n,"green":m,"yellow":k}
+    while True:
+        if num['red']+num['green']+num['yellow']==1:
+            if num['red']==1:
+                return "red"
+            elif num['green']==1:
+                return "green"
+            else:
+                return "yellow"
+        i=randint(0,2)
+        if i==0:    #当摇处红色
+            continue;
+        elif i==1:
+            if num['green']==0:
+                continue
+            else:
+                num['green']-=1; 
+                if num['red']+num['green']+num['yellow']==1:
+                    if num['red']==1:
+                        return "red"
+                    elif num['green']==1:
+                        return "green"
+                    else:
+                        return "yellow"
+                else:
+                    while True:
+                        s=randint(0,2);
+                        if s==0 and num['red']!=0:
+                            num['red']-=1;
+                            break;
+                        elif s==1 and num['green']!=0:
+                            num['green']-=1;
+                            break;
+                        elif s==2 and num['yellow']!=0:
+                            num['yellow']-=1;
+                            break;
+                        else:
+                            continue
+        elif i==2:
+            if num['yellow']==0:
+                continue;
+            else:
+                num['yellow']-=1;
+
+if __name__=="__main__":
+    m,n,k=0,0,0;
+    for _ in range(0,1000000):
+        result=Probability(1,2,3)
+        if result=="red":
+            m+=1;
+        elif result=="green":
+            n+=1;
+        elif result=="yellow":
+            k+=1;
+        else:
+            print("出现错误")
+    acount=m+n+k;
+    pa=m/acount;
+    pb=n/acount;
+    pc=k/acount;
+    print("red %0.2f green %0.2f yellow %0.2f"%(pa,pb,pc))             
