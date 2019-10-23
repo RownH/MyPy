d52786c3a7f86b74d6d9685f7b362eb352f231a7
rownh <799698579@qq.com>
2019-10-23 23:44:51 +0800

    charpt21

--- py_fluent/chart20/demo3.py
+++ py_fluent/chart20/demo3.py
@@ -45,59 +45,61 @@ class Managed:#托管类
     over_no_get=OverridingNotGet()#覆盖型没有get
     non_over=NonOverriding()
     def spm(self):
-        print('-> Managed.spam({})'.format(display(self)))    
-obj=Managed()
-print(obj.over) #obj.over触发描述符的__get__方法,第二个参数的托管实例是obj
-print(Managed.over)#触发描述符的__get__方法,第二个参数为None
-obj.over=7;#触发set 最后一个参数为7
-print(obj.over)# 读取obj.over 仍会触发描述符的__get__方法
-print(vars(obj))    #实例中 并没有参数
-print(vars(Managed))
-obj.__dict__['over']=8;
-print(vars(obj))
-print(obj.over)#仍然Managed.over仍会覆盖
+        print('-> Managed.spam({})'.format(display(self)))   
+if __name__ == "__main__":
+        
+    obj=Managed()
+    print(obj.over) #obj.over触发描述符的__get__方法,第二个参数的托管实例是obj
+    print(Managed.over)#触发描述符的__get__方法,第二个参数为None
+    obj.over=7;#触发set 最后一个参数为7
+    print(obj.over)# 读取obj.over 仍会触发描述符的__get__方法
+    print(vars(obj))    #实例中 并没有参数
+    print(vars(Managed))
+    obj.__dict__['over']=8;
+    print(vars(obj))
+    print(obj.over)#仍然Managed.over仍会覆盖
 
 
-# 没有get的方法 
-'''
-    因为没有处理读操作的__get__方法.如果直接通过实例的__dict__属性创建同名实例属性,以后在设置那个属性.就会直接从实例中返回新赋予的值
-    而不会返回描述符对象.也就是说,实例属性会遮盖描述符,不过只有读操作如此.
+    # 没有get的方法 
+    '''
+        因为没有处理读操作的__get__方法.如果直接通过实例的__dict__属性创建同名实例属性,以后在设置那个属性.就会直接从实例中返回新赋予的值
+        而不会返回描述符对象.也就是说,实例属性会遮盖描述符,不过只有读操作如此.
 
-'''
+    '''
 
-print(obj.over_no_get)
-print(Managed.over_no_get)
-obj.over_no_get=7
-obj.__dict__['over_no_get']=9;
+    print(obj.over_no_get)
+    print(Managed.over_no_get)
+    obj.over_no_get=7
+    obj.__dict__['over_no_get']=9;
 
-print(obj.over_no_get)
-print(Managed.over_no_get)
-obj.__dict__['over_no_get']=9;
-print(vars(obj))
-print(obj.over_no_get)  #__dict创建同名实例,直接从实例中返回值
-obj.over_no_get=7;
-print(obj.over_no_get)  #__set__没有修改属性 所以在此读取obj.over_no_get获取的仍是托管类的描述符实例
-#通过__dict 可以设置实例属性,通过__set__不能
+    print(obj.over_no_get)
+    print(Managed.over_no_get)
+    obj.__dict__['over_no_get']=9;
+    print(vars(obj))
+    print(obj.over_no_get)  #__dict创建同名实例,直接从实例中返回值
+    obj.over_no_get=7;
+    print(obj.over_no_get)  #__set__没有修改属性 所以在此读取obj.over_no_get获取的仍是托管类的描述符实例
+    #通过__dict 可以设置实例属性,通过__set__不能
 
-'''
-    没有实现__set__方法的描述符是非覆盖型描述符
-    如果设置了同名实例属性,描述符会被遮盖,致使描述符无法处理那个实例的那个属性
-    方法是以非覆盖型描述符实现的
-'''
-print(obj.over)
-obj.non_over=7;
-print(obj.non_over)#因为为非覆盖型描述符,这里没有干涉赋值操作哦的__set__
-print(Managed.non_over)#描述符仍然存在
-del obj.non_over
-print(obj.non_over) #会触发 覆盖型也称为强制型,非覆盖型称为遮盖型
-#不管是不是覆盖型,为类属性赋值都能覆盖描述符
+    '''
+        没有实现__set__方法的描述符是非覆盖型描述符
+        如果设置了同名实例属性,描述符会被遮盖,致使描述符无法处理那个实例的那个属性
+        方法是以非覆盖型描述符实现的
+    '''
+    print(obj.over)
+    obj.non_over=7;
+    print(obj.non_over)#因为为非覆盖型描述符,这里没有干涉赋值操作哦的__set__
+    print(Managed.non_over)#描述符仍然存在
+    del obj.non_over
+    print(obj.non_over) #会触发 覆盖型也称为强制型,非覆盖型称为遮盖型
+    #不管是不是覆盖型,为类属性赋值都能覆盖描述符
 
-#在类中覆盖描述符
-Managed.over=1;
-Managed.over_no_get=2;
-Managed.non_over=3;
-print(Managed.over,Managed.over_no_get,Managed.non_over);
-#会直接覆盖文件描述符
-#揭示了一种不对等,读类属性的操作 可以由依附在托管上定义有__get__方法的描述符处理
-#但是写类属性的操作不会由依附在托管类上定义了__set__方法的描述符处理
+    #在类中覆盖描述符
+    Managed.over=1;
+    Managed.over_no_get=2;
+    Managed.non_over=3;
+    print(Managed.over,Managed.over_no_get,Managed.non_over);
+    #会直接覆盖文件描述符
+    #揭示了一种不对等,读类属性的操作 可以由依附在托管上定义有__get__方法的描述符处理
+    #但是写类属性的操作不会由依附在托管类上定义了__set__方法的描述符处理
 
--- py_fluent/chart20/demo4.py
+++ py_fluent/chart20/demo4.py
@@ -1,4 +1,29 @@
 '''
     20.3 如果方法是描述符
-    
+    在类中定义的函数属于绑定方法
+    因为用户定义的函数都有__get__方法.所以依附在类上时,就相当于描述符
+
+
+from demo3 import Managed;
+obj=Managed();
+print(obj.spm)  #获得绑定方法的对象
+print(Managed.spm)#获得函数
+obj.spm=7;  #遮盖类属性
+print(obj.spm)  #没有实现__set__ 因此是非覆盖型描述符
+#通过托管类访问,函数的__get__方法会返回自身的引用  但是通过实例 __get__返回的是绑定方法对象
 '''
+import collections
+class Text(collections.UserString):
+
+    def __repr__(self):
+        return 'Text({ ! r })'.format(self.data)
+    def reverse(self):
+        return self[::-1];
+
+word=Text('forword');
+print(word)
+print(word.reverse())
+print(Text.reverse(Text('hello')))
+print(type(word),type(Text.reverse)) #一个类属性  一个方法
+print(Text.reverse.__get__(word))
+print(Text.reverse.__get__(none,Text))
