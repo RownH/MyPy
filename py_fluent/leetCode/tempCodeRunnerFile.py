497cbe703b19ea0d9a066ff93964c1ac0adb9eb9
rownh <799698579@qq.com>
2019-10-31 23:41:25 +0800

    时间超过

--- py_fluent/leetCode/tempCodeRunnerFile.py
+++ py_fluent/leetCode/tempCodeRunnerFile.py
@@ -1,13 +1,26 @@
-class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        length = len(grid)
-        width = len(grid[0])
-        prm = 0
-        for i in range(length):
-            for j in range(width):
-                if grid[i][j] == 1:
-                    if j == 0 or grid[i][j - 1] == 0:
-                        prm += 1
-                    if i == 0 or grid[i - 1][j] == 0:
-                        prm += 1
-        return prm * 2
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        sum=0;
+        for i in grid:
+            for j in i:
+                sum+=self.count(grid,i,j);
+        return sum;
+
+    def count(grid,i,j):
+        cnt=0;
+        if grid[i][j]==0:
+            return 0;
+        else:
+            if i==0 || grid[i-1][j]==0:
+                cnt+=1;
+            if i==len(grid[i])-1 || grid[i+1][j]==0:
+                cnt+=1;
+            if j==0||grid[i][j-1]==0:
+                cnt+=1;
+            if j==len(grid)-1 || grid[i][j+1]==0:
+                cnt+=1;
+        return cnt;
