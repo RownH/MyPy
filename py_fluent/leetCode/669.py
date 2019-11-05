3dd72dc588e6e771829d8a810cef3fc1d10921be
rownh <799698579@qq.com>
2019-11-05 23:33:50 +0800

    669

---
+++ py_fluent/leetCode/669.py
@@ -0,0 +1,64 @@
+'''
+669. 修剪二叉搜索树
+
+
+
+
+题目描述
+评论 (83)
+题解(19)New
+提交记录
+给定一个二叉搜索树，同时给定最小边界L 和最大边界 R。通过修剪二叉搜索树，使得所有节点的值在[L, R]中 (R>=L) 。你可能需要改变树的根节点，所以结果应当返回修剪好的二叉搜索树的新的根节点。
+
+示例 1:
+
+输入: 
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+输出: 
+    1
+      \
+       2
+示例 2:
+
+输入: 
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+输出: 
+      3
+     / 
+   2   
+  /
+ 1
+
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if root.val
