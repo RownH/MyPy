43a3813aedb2643bdc165e8efa50e98756b51972
rownh <799698579@qq.com>
2019-11-25 22:44:21 +0800

    投票应用

--- pReader/poem/views.py
+++ pReader/poem/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.template import loader
 # Create your views here.
-from django.http import HttpResponse
-
-
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question
+from django.urls import reverse
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('poems/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+def detail(request, question_id):
+    question=get_object_or_404(Question,pk=question_id);
+    return render(request, 'poems/detail.html', {'question': question})
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poems/results.html', {'question': question})
+def vote(request, question_id):
+    question=get_object_or_404(Question,pk=question_id);
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'poems/detail.html',{
+            'question':question,
+            'error_message':"You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes+=1;
+        selected_choice.save();
+    return  HttpResponseRedirect(reverse('poem:results',args=(question_id,)))
