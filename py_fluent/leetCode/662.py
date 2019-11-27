7e750363a4e9451d642c8824eda9962572e09cf0
rownh <799698579@qq.com>
2019-11-28 00:02:45 +0800

    遍历

---
+++ py_fluent/leetCode/662.py
@@ -0,0 +1,90 @@
+'''
+
+会员中心
+3
+
+ 
+662. 二叉树最大宽度
+
+
+
+
+题目描述
+评论 (84)
+题解(17)New
+提交记录
+给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
+
+每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
+
+示例 1:
+
+输入: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+输出: 4
+解释: 最大值出现在树的第 3 层，宽度为 4 (5,3,null,9)。
+示例 2:
+
+输入: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+输出: 2
+解释: 最大值出现在树的第 3 层，宽度为 2 (5,3)。
+示例 3:
+
+输入: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+输出: 2
+解释: 最大值出现在树的第 2 层，宽度为 2 (3,2)。
+示例 4:
+
+输入: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+输出: 8
+解释: 最大值出现在树的第 4 层，宽度为 8 (6,null,null,null,null,null,null,7)。
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        queue = [(root, 0, 0)]
+        cur_depth = left = ans = 0
+        for node, depth, pos in queue:
+            if node:
+                queue.append((node.left, depth+1, pos*2))
+                queue.append((node.right, depth+1, pos*2 + 1))
+                if cur_depth != depth:
+                    cur_depth = depth
+                    left = pos
+                ans = max(pos - left + 1, ans)
+
+        return ans
