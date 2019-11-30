fa1960bfdc4395c46799d4db390ab2e3ccd1d8b2
rownh <799698579@qq.com>
2019-11-30 23:57:34 +0800

    未理解

---
+++ py_fluent/leetCode/894.py
@@ -0,0 +1,47 @@
+'''
+894. 所有可能的满二叉树
+
+
+
+
+题目描述
+评论 (32)
+题解(11)New
+提交记录
+满二叉树是一类二叉树，其中每个结点恰好有 0 或 2 个子结点。
+
+返回包含 N 个结点的所有可能满二叉树的列表。 答案的每个元素都是一个可能树的根结点。
+
+答案中每个树的每个结点都必须有 node.val=0。
+
+你可以按任何顺序返回树的最终列表。
+
+
+'''
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    memo = {0: [], 1: [TreeNode(0)]}
+
+    def allPossibleFBT(self, N):
+        if N not in Solution.memo:
+            ans = []
+            for x in xrange(N):
+                y = N - 1 - x
+                for left in self.allPossibleFBT(x):
+                    for right in self.allPossibleFBT(y):
+                        bns = TreeNode(0)
+                        bns.left = left
+                        bns.right = right
+                        ans.append(bns)
+            Solution.memo[N] = ans
+
+        return Solution.memo[N]
+
+
+
---
+++ py_fluent/leetCode/shudu.py
@@ -0,0 +1,113 @@
+import time  # 倒入时间模块
+t0 = time.time()  # 启动程序时间
+
+
+class point:  # 点
+    def __init__(self, x, y):  # 构造函数
+        self.x = x  # 初始化x,y
+        self.y = y
+        self.available = []  # 可选的点
+        self.value = 0  # 值默认为0
+
+
+def rowNum(p, sudoku):
+    row = set(sudoku[p.y*9:(p.y+1)*9])  # 取得p所在当前一行 set
+    row.remove(0)  # 除去第0项
+    return row  # 返回set(row)
+
+
+def colNum(p, sudoku):
+    col = []
+    length = len(sudoku)    #获得整个数组的大小
+    for i in range(p.x, length, 9): 
+        col.append(sudoku[i])   #获得当前点的 p所在的一列
+    col = set(col)  #去除重复值
+    col.remove(0)#去除0值
+    return col  #返回当前
+
+
+def blockNum(p, sudoku): #取得x所属块的值
+    block_x = p.x//3    #当前x余3
+    block_y = p.y//3    #当前y余3
+    block = []
+    start = block_y*3*9+block_x*3   #获得当前块的左上角点
+    for i in range(start, start+3):
+        block.append(sudoku[i]) #将第一行的值 放入数组
+    for i in range(start+9, start+9+3): #第二行值 放入
+        block.append(sudoku[i])
+    for i in range(start+9+9, start+9+9+3):#第三行值
+        block.append(sudoku[i])
+    block = set(block)  #去除重复值
+    block.remove(0) #去除0值
+    return block  # set type
+
+
+def initPoint(sudoku):  # 初始化
+    pointList = []  # 创建一个列表
+    length = len(sudoku)  # 获取数独剧组的总大小
+    for i in range(length):  # 遍历0-80个元素
+        if sudoku[i] == 0:  # 如果这个点上没有值
+            p = point(i % 9, i//9)    # y为 此点除9  x为此点余9
+            for j in range(1, 10):  # 遍历9次 1 -9 之间 选择值放入可备用数组中
+                if j not in rowNum(p, sudoku) and j not in colNum(p, sudoku) and j not in blockNum(p, sudoku):  # 如果1-9递增的值  不再当前行  当前列  当前块中
+                    p.available.append(j)   #把这个值放入数组中
+            pointList.append(p)    #把整个数组添加进数组中
+    return pointList #返回整个数组中 每一项为0的点 坐标值以及为可选的点
+
+
+def tryInsert(p, sudoku):
+    availNum = p.available  
+    for v in availNum:
+        p.value = v
+        if check(p, sudoku):
+            sudoku[p.y*9+p.x] = p.value #把该点放入在数组中
+            if len(pointList) <= 0: #如果当前队列中已经不存在点
+                t1 = time.time()    #获得结束时间
+                useTime = t1-t0     #两次时间相减
+                showSudoku(sudoku)  #显示递归后的结果
+                print('\nuse Time: %f s' % (useTime))   #显示使用时间
+                exit()  #退出
+            p2 = pointList.pop()    #弹出第二个点
+            tryInsert(p2, sudoku)   #递归插入 插入第二个点
+            sudoku[p2.y*9+p2.x] = 0 #数独的第(y,x)点和(x,y)点 赋值为0 如果有一次插入失败时 需要递归 修改之前状态为0
+            sudoku[p.y*9+p.x] = 0
+            p2.value = 0    #值为0
+            pointList.append(p2)    #将p2添加至末尾
+        else:   #重新加入
+            pass
+
+
+def check(p, sudoku):
+    if p.value == 0:    #如果当前点为0 返回没有值给点p
+        print('not assign value to point p!!')
+        return False
+    if p.value not in rowNum(p, sudoku) and p.value not in colNum(p, sudoku) and p.value not in blockNum(p, sudoku):
+        return True     #如果该值不存在于 当前行  或者 当前列  或者当前所属的块时  返回True
+    else:
+        return False    #否则返回假
+
+
+def showSudoku(sudoku):
+    for j in range(9):
+        for i in range(9):
+            print('%d ' % (sudoku[j*9+i]), end='')
+        print('')
+
+
+if __name__ == '__main__':
+    sudoku = [
+        8, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 3, 6, 0, 0, 0, 0, 0,
+        0, 7, 0, 0, 9, 0, 2, 0, 0,
+        0, 5, 0, 0, 0, 7, 0, 0, 0,
+        0, 0, 0, 0, 4, 5, 7, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 3, 0,
+        0, 0, 1, 0, 0, 0, 0, 6, 8,
+        0, 0, 8, 5, 0, 0, 0, 1, 0,
+        0, 9, 0, 0, 0, 0, 4, 0, 0,
+    ]  # 数独列表
+    pointList = initPoint(sudoku)   #初始化每个点  大概能放那些值
+    showSudoku(sudoku)  #打印出当前数组的样子
+    print('\n')
+    p = pointList.pop() #弹出最后一项
+    tryInsert(p, sudoku)    #递归插入点
